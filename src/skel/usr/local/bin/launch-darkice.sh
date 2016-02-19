7131648b66f52d9ddda0c326d067c9be033e69f0
Brian Lalor <blalor@bravo5.org>
2016-02-19 14:38:57 -0500

    Reduce the capture volume on the line input
    With the sonos output fixed, any boost is too much. mad distortion, yo.

--- src/skel/usr/local/bin/launch-darkice.sh
+++ src/skel/usr/local/bin/launch-darkice.sh
@@ -7,8 +7,11 @@ lsusb
 cat /proc/asound/cards
 arecord -l
 
-## configure S/PDIF in
+## configure the mixer
 amixer -c Device set 'PCM Capture Source' "${PCM_CAPTURE_SOURCE:-IEC958 In}"
+
+## defaults to something quite high which is distorted as fuck
+amixer -c Device set 'Line,0,Capture' 0
 
 ## generate config
 sed \
